9e31ee5ff6a1f9200db2ac24b17779351cb64bed
apartgecko88165 <apartgecko88165@gmail.com>
2021-10-16 16:40:28 -0700

    a few updates, the image buttons on the home page now work better

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,12 @@
+const joinPath = "../html/join.html";
+const viewPath = "../html/view.html";
+
 // call functions to change element href to correct location
 document.getElementById("join-exchange").onclick =
-() => { location.href = "../html/join.html"; };
+() => { location.href = joinPath; };
 document.getElementById("view-exchanges").onclick =
-() => { location.href = "../html/view.html"; };
+() => { location.href = viewPath; };
+document.getElementById("join-text-button").onclick =
+() => { location.href = joinPath; };
+document.getElementById("view-text-button").onclick =
+() => { location.href = viewPath; };
