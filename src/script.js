982beff81df137e5957864e39969823e2ef3f9fe
apartgecko88165 <apartgecko88165@gmail.com>
2021-10-16 12:25:39 -0700

    created repo, just has some starting html and scss

---
+++ src/script.js
@@ -0,0 +1,2 @@
+document.getElementById("join-exchange").onclick = () => { location.href = "../html/join.html"; };
+document.getElementById("view-exchanges").onclick = () => { location.href = "../html/view.html"; };
